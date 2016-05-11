014326cd25740314ef8782de0a41f124a53150a4
Bac <ben@joomexp.com>
2016-05-11 14:28:47 +0700

    update tmpl

--- header.php
+++ header.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Wind
- * Date: 11/05/2016
- * Time: 13:49
- */
+
+
+if(!isset($title)){
+    $title ="Upload ảnh";
+}
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <script src="public/js/jquery-2.0.0.js"></script>
+    <script src="public/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/public.css">
+    <script src="public/js/public.js"></script>
+</head>
+<body>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Wind
- * Date: 11/05/2016
- * Time: 13:49
- */
+
+
+<?php include_once "header.php"; ?>
+<div class="container col-sm-6 col-sm-offset-3">
+    <form role="form">
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+        </div>
+        <div class="checkbox">
+            <label><input type="checkbox"> Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+</div>
+<?php include_once "footer.php" ?>
